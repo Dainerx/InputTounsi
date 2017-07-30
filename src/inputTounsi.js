81cdeb1f9ef7b2cb38ba2ee865f16f124887130d
Dainerx <oussema.benghorbel@esprit.tn>
2017-07-30 01:18:59 +0100

    Beta 0.0

---
+++ src/inputTounsi.js
@@ -0,0 +1,235 @@
+var data = '[{ "state": "Ben Arous","cities": ["Ben Arous", "El Mourouj", "Hammam Lif", "Hammam Chott", "Bou Mhel el-Bassatine", "Ezzahra", "Radès", "Mégrine", "Mohamedia-Fouchana", "Mornag", "Khalidia"]},{"state": "Tunis","cities": ["Tunis", "Le Bardo", "Le Kram", "La Goulette", "Carthage", "Sidi Bou Said", "La Marsa", "Sidi Hassine"]},{"state": "Ariana","cities": ["Ariana", "La Soukra", "Raoued", "Kalâat el-Andalous", "Sidi Thabet", "Ettadhamen-Mnihla"]},{"state": "Manouba","cities": ["Manouba", "Den Den", "Douar Hicher", "Oued Ellil", "Mornaguia", "Borj El Amri", "Djedeida", "Tebourba", "El Battan"]},{ "state": "Nabeul","cities": ["Nabeul", "Dar Chaabane", "Béni Khiar", "El Maâmoura", "Somâa", "Korba", "Tazerka", "Menzel Temime", "Menzel Horr", "El Mida", "Kelibia","Azmour","Hammam Ghezèze", "Dar Allouch", "El Haouaria", "Takelsa", "Soliman",  "Korbous", "Menzel Bouzelfa", "Béni Khalled", "Zaouiet Djedidi", "Grombalia", "Bou Argoub", "Hammamet"]},{ "state": "Zaghouan","cities": ["Zaghouan", "Zriba", "Bir Mcherga", "Djebel Oust", "El Fahs", "Nadhour"]},{ "state": "Bizerte","cities": ["Bizerte", "Sejnane", "Mateur", "Menzel Bourguiba", "Tinja", "Ghar al Milh", "Aousja", "Menzel Jemil", "Menzel Abderrahmane", "El Alia", "Ras Jebel", "Metline", "Raf Raf"]},{ "state": "Béja","cities": ["Béja", "El Maâgoula", "Zahret Medien", "Nefza", "Téboursouk", "Testour", "Goubellat", "Majaz al Bab"]},{ "state": "Jendouba","cities": ["Jendouba", "Bou Salem", "Tabarka", "Aïn Draham", "Fernana", "Beni Mtir", "Ghardimaou", "Oued Melliz"]},{ "state": "El Kef","cities": ["El Kef", "Nebeur", "Touiref", "Sakiet Sidi Youssef", "Tajerouine", "Menzel Salem", "Kalaat es Senam", "Kalâat Khasba", "Jérissa", "El Ksour", "Dahmani", "Sers"]},{ "state": "Siliana","cities": ["Siliana", "Bou Arada", "Gaâfour", "El Krib", "Sidi Bou Rouis", "Maktar", "Rouhia", "Kesra", "Bargou", "El Aroussa"]},{ "state": "Sousse","cities": ["Sousse", "Ksibet Thrayet", "Ezzouhour", "Zaouiet Sousse", "Hammam Sousse", "Akouda", "Kalâa Kebira", "Sidi Bou Ali", "Hergla", "Enfidha", "Bouficha", "Sidi El Hani", "Msaken", "Kalâa Seghira", "Messaadine", "Kondar"]},{ "state": "Monastir","cities": ["Monastir", "Khniss", "Ouerdanin", "Sahline Moôtmar", "Sidi Ameur", "Zéramdine", "Beni Hassen", "Ghenada", "Jemmal", "Menzel Kamel", "Zaouiet Kontoch", "Bembla-Mnara", "Menzel Ennour", "El Masdour", "Moknine", "Sidi Bennour", "Menzel Farsi",  "Amiret El Fhoul", "Amiret Touazra", "Amiret El Hojjaj", "Cherahil", "Bekalta", "Téboulba", "Ksar Hellal", "Ksibet El Mediouni", "Benen Bodher", "Touza", "Sayada", "Lemta", "Bouhjar", "Menzel Hayet"]},{ "state": "Mahdia","cities": ["Mahdia", "Rejiche", "Bou Merdes", "Ouled Chamekh", "Chorbane", "Hebira", "Essouassi", "El Djem", "Kerker", "Chebba", "Melloulèche", "Sidi Alouane", "Ksour Essef", "El Bradâa"]},{ "state": "Sfax","cities": ["Sfax", "Sakiet Ezzit", "Chihia", "Sakiet Eddaïer", "Gremda", "El Ain", "Thyna", "Agareb", "Jebiniana", "El Hencha", "Menzel Chaker", "Ghraïba", "Bir Ali Ben Khélifa", "Skhira", "Mahares", "Kerkennah"]},{ "state": "Kairouan","cities": ["Kairouan", "Chebika", "Sbikha", "Oueslatia", "Aïn Djeloula", "Haffouz", "Alaâ", "Hajeb El Ayoun", "Nasrallah", "Menzel Mehiri", "Echrarda", "Bou Hajla"]},{ "state": "Kasserine","cities": ["Kasserine", "Sbeitla", "Sbiba", "Jedelienne", "Thala", "Haïdra", "Foussana", "Fériana", "Thélepte", "Magel Bel Abbès"]},{ "state": "Sidi Bouzid","cities": ["Sidi Bouzid", "Jilma", "Cebalet", "Bir El Hafey", "Sidi Ali Ben Aoun", "Menzel Bouzaiane", "Meknassy", "Mezzouna", "Regueb", "Ouled Haffouz"]},{ "state": "Gabès","cities": ["Gabès", "Chenini Nahal", "Ghannouch", "Métouia", "Oudhref", "El Hamma", "Matmata", "Nouvelle Matmata", "Mareth", "Zarat"]},{ "state": "Mednine","cities": ["Medenine", "Beni Khedache", "Ben Gardane", "Zarzis", "Houmt El Souk (Djerba)", "Midoun (Djerba)", "Ajim (Djerba)"]},{ "state": "Tataouine","cities": ["Tataouine", "Bir Lahmar", "Ghomrassen", "Dehiba", "Remada"]},{ "state": "Gafsa","cities": ["Gafsa", "El Ksar", "Moularès", "Redeyef", "Métlaoui", "Mdhila", "El Guettar", "Sened"]},{ "state": "Tozeur","cities": ["Tozeur", "Degache", "Hamet Jerid", "Nafta", "Tamerza"]},{ "state": "Kebili", "cities": ["Kebili", "Djemna", "Douz", "El Golâa", "Souk Lahad"]}]';
+
+function findCities(state) {
+    var dataParsed = JSON.parse(data);
+    for (var i = dataParsed.length - 1; i >= 0; i--) {
+        if (dataParsed[i].state == state) return dataParsed[i].cities;
+    }
+    return {};
+}
+
+var verifyFunctions = [];
+verifyFunctions['cin'] = function (str) {
+    if (str instanceof Array) {
+        if (str["pattern"] != undefined) {
+            return str['pattern'].test(str['value']);
+        }
+        else
+            return /^(1|0)\d{6}$/.test(str);
+    }
+    return /^(1|0)\d{6}$/.test(str);
+};
+verifyFunctions['name'] = function (str) {
+    if (str instanceof Array) {
+        if (str["pattern"] != undefined) {
+            return str['pattern'].test(str['value']);
+        }
+        else
+            return /^[A-Za-z ]{4,50}$/.test(str['value']);
+    }
+    return /^[A-Za-z ]{4,50}$/.test(str);
+};
+verifyFunctions['lastname'] = function (str) {
+    if (str instanceof Array) {
+        if (str["pattern"] != undefined) {
+            return str['pattern'].test(str['value']);
+        }
+        else
+            return /^[A-Za-z ]{4,50}$/.test(str);
+    }
+    return /^[A-Za-z ]{4,50}$/.test(str);
+};
+verifyFunctions['phone'] = function (str) {
+    if (str instanceof Array) {
+        if (str["pattern"] != undefined) {
+            return str['pattern'].test(str['value']);
+        }
+        else
+            return /^(2|5|9)\d{7}$/.test(str);
+    }
+    return /^(2|5|9)\d{7}$/.test(str);
+};
+verifyFunctions['email'] = function (str) {
+    if (str instanceof Array) {
+        if (str["pattern"] != undefined) {
+            return str['pattern'].test(str['value']);
+        }
+        else
+            return /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(str);
+    }
+    return /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(str);
+};
+verifyFunctions['state'] = function (box, boxCity) {
+
+    var dataParsed = JSON.parse(data);
+    for (var i = 0; i < dataParsed.length; i++) {
+        var o = new Option(dataParsed[i].state, dataParsed[i].state);
+        $(o).html(dataParsed[i].state);
+        $(box).append(o);
+    }
+
+    $(box).on('change', function () {
+        $(boxCity)
+            .find('option')
+            .remove()
+            .end();
+        $(boxCity).prop('disabled', false);
+        console.log("my object: %o", findCities($(box).val()));
+
+        for (var i = findCities($(box).val()).length - 1; i >= 0; i--) {
+            var o = new Option(findCities($(box).val())[i], findCities($(box).val())[i]);
+            $(o).html(findCities($(box).val())[i]);
+            $(boxCity).append(o);
+        }
+    });
+
+};
+verifyFunctions['city'] = function (box) {
+    $(box).prop('disabled', true);
+};
+
+verifyFunctions['address'] = function (str) {
+    if (str instanceof Array) {
+        if (str["pattern"] != undefined) {
+            return str['pattern'].test(str['value']);
+        }
+        else
+            return /^\d+\s[A-z]+\s[A-z]+/.test(str);
+    }
+    return /^\d+\s[A-z]+\s[A-z]+/.test(str);
+};
+
+(function ($) {
+
+    $.fn.verifyInputTounsi = function (type, typeChild) {
+
+        return this.each(function () {
+            var input = $(this);
+
+            if (type instanceof Array) {
+
+                if (type["placeholder"] != undefined)
+                    input.attr("placeholder", type['placeholder']);
+                else
+                    input.attr("placeholder", "Your " + type['name']);
+
+                input.on("change", function () {
+                    type['value'] = input.val();
+
+                    if (verifyFunctions[type['name']](type)) {
+                        if (type["messageError"] != undefined) {
+                            if ($("#" + type['name'] + "Error").length)
+                                $("#" + type['name'] + "Error").remove();
+                        }
+                        else {
+                            if ($("#" + type + "Error").length)
+                                $("#" + type + "Error").remove();
+                        }
+
+                        var o = {};
+
+                        if (type["styleSuccess"] != undefined) {
+                            for (var key in type['styleSuccess']) {
+                                o[key] = type['styleSuccess'][key];
+                            }
+                        }
+
+                        else {
+                            o['border-style'] = 'solid';
+                            o['border-color'] = 'green';
+                        }
+
+                        input.css(o);
+                    }
+                    else {
+
+                        if (type["messageError"] != undefined) {
+                            if ($("#" + type['name'] + "Error").length)
+                                $("#" + type['name'] + "Error").remove();
+                        }
+                        else {
+                            if ($("#" + type + "Error").length)
+                                $("#" + type + "Error").remove();
+                        }
+
+                        var o = {};
+
+                        if (type["styleError"] != undefined) {
+                            for (var key in type['styleError']) {
+                                o[key] = type['styleError'][key];
+                            }
+                        }
+
+                        else {
+                            o['border-style'] = 'solid';
+                            o['border-color'] = 'red';
+                        }
+
+                        input.css(o);
+
+                        var o1 = {};
+
+                        if (type["messageErrorStyle"] != undefined) {
+                            for (var key in type['messageErrorStyle']) {
+                                o1[key] = type['messageErrorStyle'][key];
+                            }
+                        }
+                        else {
+                            o1['color'] = 'red';
+                        }
+
+                        input.after(function () {
+                            if (type["messageError"] != undefined) {
+                                var labelAfter = $("<label>").text(type['messageError']);
+                                labelAfter.attr({id: type['name'] + "Error"});
+                            }
+                            else {
+                                var labelAfter = $("<label>").text("The " + type['name'] + " you entered does not meet the Tunisian criteria");
+                                labelAfter.attr({id: type + "Error"});
+                            }
+
+                            labelAfter.css(o1);
+                            return labelAfter;
+                        });
+
+                    }
+                });
+            }
+
+            else {
+                if (typeof verifyFunctions[type](input.val()) == "boolean") {
+                    input.attr("placeholder", "Your " + type);
+                    input.on("change", function () {
+                        if (verifyFunctions[type](input.val())) {
+                            if ($("#" + type + "Error").length) {
+                                $("#" + type + "Error").remove();
+                            }
+                            input.css({"border-style": "solid", "border-color": "green"});
+                        }
+                        else {
+                            input.css({"border-style": "solid", "border-color": "red"});
+                            if ($("#" + type + "Error").length) {
+                                $("#" + type + "Error").remove();
+                            }
+                            input.after(function () {
+                                var labelAfter = $("<label>").text("The " + type + " you entered does not meet the Tunisian criteria");
+                                labelAfter.css({"color": "red"});
+                                labelAfter.attr({id: type + "Error"});
+                                return labelAfter;
+                            });
+                        }
+                    });
+                }
+                else {
+                    verifyFunctions[type](input, typeChild);
+                }
+            }
+        });
+
+        return this;
+
+    };
+
+}(jQuery));
+
+
